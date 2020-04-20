3c0176bb982da2c85d384278fa0d1e6faedcdfec
pedro <pedro@>
2020-04-20 18:10:56 +0200

    Creando el main

--- templates/main.php
+++ templates/main.php
@@ -1,17 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="libs\bootstrap-4.4.1-dist\css\bootstrap.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Handlee&display=swap');
+    </style>
     <title>Document</title>
 </head>
+
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-9 border">sdfsdf</div>
+            <div class="col-sm-4 mt-4 ml-4 p-4 hoja1">
+                <li>Comida</li>
+                <li>Reunión</li>
+            </div>
+            <div class="col-sm-4 mt-4 ml-4 p-4 hoja2">
+                <li>Comida</li>
+                <li>Reunión</li>
+            </div>
+            <div class="col-sm-3 mt-4 ml-4 p-4 details">
+            </div>
         </div>
     </div>
 </body>
+
 </html>
