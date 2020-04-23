584a89509d1696fe756752b72baf3c6757a729da
pedro <pedro@>
2020-04-23 20:10:34 +0200

    Terminando Filas de las Listas

--- templates/main.php
+++ templates/main.php
@@ -6,23 +6,179 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="libs\bootstrap-4.4.1-dist\css\bootstrap.css">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Handlee&display=swap');
-    </style>
-    <title>Document</title>
+    <script src="https://kit.fontawesome.com/32099913fe.js" crossorigin="anonymous"></script>
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Handlee&display=swap');
+</style>
+<title>Document</title>
 </head>
 
 <body>
     <div class="container-fluid">
         <div class="row">
+            <!-- Inicio Tarjeta 1 -->
             <div class="col-sm-4 mt-4 ml-4 p-4 hoja1">
-                <li>Comida</li>
-                <li>Reunión</li>
+                <div class="fecha1">
+                    <p>Hoy</p>
+                </div>
+                <ul>
+                    <div>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Comida con la familia, ya no se que mas poner pero debo poner cosas hasta que salte de linea</p>
+                            <div class="iconos"><i class="far fa-square importancia"></i>
+                                <i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <li>
+                            <p>12:00 - Reunión</p>
+                            <div class="iconos"> <i class="far fa-square importancia"></i> <i class="iconos fas fa-trash-alt"></p></i><i class="iconos fas fa-pencil-alt"></i></div>
+                        </li>
+                        <i class="fas fa-plus"></i>
+
+                    </div>
+                </ul>
             </div>
+            <!-- Final Tarjeta 1 -->
+            <!-- Inicio Tarjeta 2 -->
             <div class="col-sm-4 mt-4 ml-4 p-4 hoja2">
-                <li>Comida</li>
-                <li>Reunión</li>
+                <div class="fecha2">
+                    <p>Mañana</p>
+                </div>
+                <ul>
+                    <li>
+                        <p>12:00 - Comida </p>
+                        <div class="iconos"><i class="far fa-square importancia"></i><i class="iconos fas fa-trash-alt"></i><i class="iconos fas fa-pencil-alt"></i></div>
+                    </li>
+                    <i class="fas fa-plus"></i>
+                </ul>
             </div>
+            <!-- Final Tarjeta 2 -->
             <div class="col-sm-3 mt-4 ml-4 p-4 details">
             </div>
         </div>
